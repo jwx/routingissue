3a2ed4f1ecd17e9b376bd4a84f474d326de6fda8
ad <ad@datawerk.com>
2021-08-12 15:39:18 +0200

    second commit

--- src/editor-view.ts
+++ src/editor-view.ts
@@ -1,9 +1,4 @@
-import {inject, bindable} from 'aurelia';
-@inject()
+
 export class EditorView{
-    
-    @bindable show: boolean = true;
-    showId(){
-        console.log("EDITOR: ", this.show)
-    }
+   
 }
